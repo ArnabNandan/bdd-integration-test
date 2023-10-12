be2a88cf683e9b1ee3d8141008bc64d352f2fcda
Arnab Nandan <arnabnandan@gmail.com>
2023-10-12 13:21:59 +0530

    updates

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -1,15 +1,17 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @RunWith(Cucumber.class)
+@ActiveProfiles("prod")
 @CucumberOptions(features = {"classpath:features"},
         plugin = {"pretty", "html:target/cucumber/test-report",
                 "pretty:target/cucumber-pretty.txt",
                 "usage:target/cucumber-usage.json"},
-        // publish = true,
-        // tags= "@SmokeTest",
+        publish = true,
+        //tags = "@SmokeTest",
         extraGlue = "com.example.bddintegrationtest.common")
 public class CucumberRunnerTest {
 }
